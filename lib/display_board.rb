52a13e104eac33df73c35e377076d15509ba0191
Hillevi Eklöw <hillevieklow@gmail.com>
2018-03-27 23:37:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board)
+def display_board(brd)
+  puts brd
 end
 
 board = [" "," "," "," "," "," "," "," "," "]
